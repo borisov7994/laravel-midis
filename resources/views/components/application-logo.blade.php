f85a4cd2efb2009aebe18030d209a68e6ec75f27
borisov7994 <121038660+borisov7994@users.noreply.github.com>
2025-06-16 14:18:42 +0300

    Update application-logo.blade.php

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,12 +1,8 @@
 <svg viewBox="0 0 316 316" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
     <path d="M158 20L195 126L308 126L214 190L251 296L158 230L65 296L102 190L8 126L121 126Z" 
           fill="#4F46E5" stroke="#312E81" stroke-width="5"/>
-    
-    <!-- Внутренний декоративный элемент -->
     <path d="M158 80L182 140L248 140L198 180L222 240L158 200L94 240L118 180L68 140L134 140Z" 
           fill="white" stroke="#312E81" stroke-width="2"/>
-    
-    <!-- Акцентные линии -->
     <path d="M158 40L180 90M158 40L136 90M250 150L280 120M250 150L280 180M66 150L36 120M66 150L36 180" 
           stroke="#6366F1" stroke-width="3" stroke-linecap="round"/>
-        </svg>
+</svg>
